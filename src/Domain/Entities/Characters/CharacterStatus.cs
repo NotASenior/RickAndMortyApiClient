23380939bb3f1b6331bd62c995d5066ad1a65ff6
Frank Silva Castro <frank.silva.castro.1@gmail.com>
2022-04-13 18:55:27 -0500

    removed some unused code in entities

--- src/Domain/Entities/Characters/CharacterStatus.cs
+++ src/Domain/Entities/Characters/CharacterStatus.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Entities.Characters
+﻿namespace Entities.Characters
 {
     public enum CharacterStatus
     {
--- src/Domain/Entities/Episodes/Episode.cs
+++ src/Domain/Entities/Episodes/Episode.cs
@@ -1,17 +1,11 @@
 ﻿using Entities.CrossCutting;
-using System.Runtime.Serialization;
 
 namespace Entities.Episodes
 {
-    [DataContract]
     public class Episode : EntityBase
     {
         public string? Name { get; set; }
-        
-        [DataMember(Name = "air_date")]
         public string? AirDate { get; set; }
-
-        [DataMember(Name = "Episode")]
         public string? Code { get; set; }
         public IEnumerable<string?>? Characters { get; set; }
     }
